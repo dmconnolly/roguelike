8c27e2b6a1419d4f1cb0df125f307cda515bea8a
dmconnolly <danny@flau.co.uk>
2017-12-04 23:28:44 +0000

    Restrict rand gen to integral types

--- src/random.hpp
+++ src/random.hpp
@@ -1,18 +1,21 @@
 #include <random>
 #include <cstdint>
 #include <algorithm>
+#include <type_traits>
 
 #include "pcg_random.hpp"
 
+/* TODO: Thread safety? */
+
 class Random {
 public:
-    template<typename T>
+    template<typename T, typename=typename std::enable_if<std::is_integral<T>::value>::type>
     static T between(const T a, const T b) {
         std::uniform_int_distribution<T> uniform_dist(std::min(a, b), std::max(a, b));
         return uniform_dist(rng);
     }
 
-    template<typename T>
+    template<typename T, typename=typename std::enable_if<std::is_integral<T>::value>::type>
     static std::vector<T> between(const T a, const T b, const std::size_t count) {
         std::vector<T> values(count);
         std::uniform_int_distribution<T> uniform_dist(std::min(a, b), std::max(a, b));
